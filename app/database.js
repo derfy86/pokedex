ff1094f377881eaf5a0a44315d00465b8fc04b3b
derfy86 <bourigeaudfrederic@yahoo.fr>
2021-11-26 13:23:14 +0100

    config bdd heroku ok

--- app/database.js
+++ app/database.js
@@ -1,8 +1,15 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    
-    user: 'pokedex'
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+    // user: process.env.PGUSER,
+    // database: process.env.PGDATABASE,
+    // port: process.env.PGPORT,
+    // password: process.env.PGPASSWORD,
+    // host: process.env.PGHOST
 });
 
 client.connect();
